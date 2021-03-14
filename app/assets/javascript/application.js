4519638b2ecce3d5aa366b110068376bb14235c9
mot <nonmot0614@gmail.com>
2021-03-14 20:55:19 +0900

    finish summernote

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,7 +1,31 @@
+
+// summernote-rails
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap
+//= require summernote/summernote-bs4.min
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
 // Loads all Semantic javascripts
 //= require semantic-ui
 
 // ドロップダウンを使用するためのコード
-$(function () {
-    $('.ui.dropdown').dropdown();
+$(document).on('turbolinks:load', function() {
+    $(function() {
+        $('.ui.dropdown').dropdown();
+    });
+});
+
+
+// summernoteの初期設定
+$(document).on('turbolinks:load', function() {
+    $('[data-provider="summernote"]').each(function() {
+        $(this).summernote({
+            lang: 'ja-JP',
+            height: 300,
+        });
+    });
 });
