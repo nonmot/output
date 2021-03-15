f941265e91ca3742348e92176f5a82f302ef52d8
mot <nonmot0614@gmail.com>
2021-03-15 19:13:27 +0900

    change gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,14 +69,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'whispering-harbor-21779.herokuapp.com'
-  config.action_mailer.default_url_options = {host: host}
+  config.action_mailer.default_url_options = {host: host, protocol: 'https'}
   ActionMailer::Base.smtp_settings = {
-    :port => ENV['MAILGUN_SMTP_PORT'],
-    :address => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-    :password => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain => host,
+    :port => '587',
+    :address => 'smtp.gmail.com',
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :domain => 'gmail.com',
     :authentication => :plain,
+    enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
