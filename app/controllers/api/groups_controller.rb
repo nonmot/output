b36fc8185466baf7713710cbc02df1ead201a250
mot <nonmot0614@gmail.com>
2021-04-05 18:29:05 +0900

    group creation

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -2,6 +2,7 @@ class Api::GroupsController < ApplicationController
 
     def index
         groups = Group.order('created_at DESC')
+        user = current_user
         render json: groups
     end
 
@@ -9,4 +10,20 @@ class Api::GroupsController < ApplicationController
         group = Group.find_by(id: params[:id])
         render json: group
     end
+
+    def create
+        group = Group.new(group_params)
+        group.user_id = current_user.id
+        if group.save
+            render json: {status: 'SUCCESS'}
+        else
+            render json: {status: 'ERROR'}
+        end
+    end
+
+    private
+
+        def group_params
+            params.require(:group).permit(:title, :introduction)
+        end
 end
